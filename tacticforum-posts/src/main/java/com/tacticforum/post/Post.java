64e92b8abde7743fd6ca241f5093302e974d8c0e
loic <loic.tournatory@gmail.com>
2020-11-10 15:53:14 +0100

    posts

--- tacticforum-posts/src/main/java/com/tacticforum/post/Post.java
+++ tacticforum-posts/src/main/java/com/tacticforum/post/Post.java
@@ -1,5 +1,86 @@
 package com.tacticforum.post;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+	
+	// ------------------------------------------------------------ VARIABLES 
+	@Column(name = "content")
+	@NotEmpty
+	private String content;
+	
+	@Column(name = "title")
+	@NotEmpty
+	private String title;
+	
+	@Column(name = "create_date")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate createDate;
+	
+	private Integer userId;	
+	@Transient
+	private HashSet<Integer> comments = new LinkedHashSet<>();
+	
+	// ------------------------------------------------------------ GETTERS SETTERS 
+
+	
+	// ------------------------------------------------------------ DATE 
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDate getreateDate() {
+		return this.createDate;
+	}
+	
+	// ------------------------------------------------------------ CONTENT 
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	// ------------------------------------------------------------ TITLE 
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	// ------------------------------------------------------------ COMMENTS 
+	public HashSet<Integer> getVisits() {
+		if (this.comments == null) {
+			this.comments = new HashSet<>();
+		}
+		return this.comments;
+	}
+
+	public void addVisit(Integer comments) {
+		getVisits().add(comments);
+	}
+	
+	// ------------------------------------------------------------ USERID 
+	public Integer getUser() {
+		return this.userId;
+	}
+
+	public void setUser(Integer user) {
+		this.userId = user;
+	}
 
 }
---
+++ tacticforum-posts/src/main/java/com/tacticforum/post/PostController.java
@@ -0,0 +1,79 @@
+package com.tacticforum.post;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/posts/{userId}")
+public class PostController extends Post{
+
+	private final PostRepository posts;
+
+	public PostController(PostRepository posts) {
+		this.posts = posts;
+	}
+
+	@GetMapping("/posts")
+	public Iterable<Post> getPosts() {
+		return posts.findAll();
+	}
+
+	@GetMapping("/posts/{id}")
+	public Optional<Post> getPostById(@PathVariable("id") Integer id) {
+		return posts.findById(id);
+	}
+
+	@PostMapping("/posts")
+	public Post addPost(@RequestParam("title") String title, @RequestParam("userId") Integer userId,
+			@RequestParam("content") String content, @RequestParam("createDate") LocalDate createDate) {
+		Post post = new Post();
+		post.setTitle(title);
+		post.setUser(userId);
+		post.setContent(content);
+		post.setCreateDate(createDate);
+		return posts.save(post);
+	}
+
+	@PutMapping("/posts/{id}")
+	public Post editPost(@PathVariable("id") Integer id, @RequestParam("title") String title,
+			@RequestParam("userId") Integer userId, @RequestParam("content") String content,
+			@RequestParam("createDate") LocalDate createDate) {
+
+		Optional<Post> postOpt = posts.findById(id);
+		if (postOpt.isPresent()) {
+			Post post = postOpt.get();
+			post.setTitle(title);
+			post.setUser(userId);
+			post.setContent(content);
+			post.setCreateDate(createDate);
+			posts.save(post);
+		}
+		return postOpt.get();
+	}
+
+	@DeleteMapping("/posts/{id}")
+	public void deletePet(@PathVariable("id") Integer id) {
+		posts.deleteById(id);
+	}
+
+	@PostMapping("/posts/{postId}/addComment")
+	public void addVisitToPet(@PathVariable("postId") Integer postId, @RequestParam("commentId") Integer commentId) {
+		Optional<Post> postOpt = posts.findById(postId);
+		if (postOpt.isPresent()) {
+			Post pet = postOpt.get();
+			pet.addVisit(commentId);
+			posts.save(pet);
+		}
+
+	}
+}
