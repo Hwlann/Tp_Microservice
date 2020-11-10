1756acff368825b8381fc69ee38dfc0f466162bb
loic <loic.tournatory@gmail.com>
2020-11-10 15:54:08 +0100

    posts

--- tacticforum-posts/src/main/java/com/tacticforum/post/PostRepository.java
+++ tacticforum-posts/src/main/java/com/tacticforum/post/PostRepository.java
@@ -11,5 +11,5 @@ import org.springframework.transaction.annotation.Transactional;
 public interface PostRepository extends CrudRepository<Post, Integer> {
 	@Query("SELECT DISTINCT post FROM Post post WHERE post.title LIKE :title%")
 	@Transactional(readOnly = true)
-	Collection<Post> findByLastName(@Param("title") String title);
+	Collection<Post> findByTitle(@Param("title") String title);
 }
